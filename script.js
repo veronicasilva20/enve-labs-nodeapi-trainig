d2f7101507a8ef1bd376e99b47957b568310fc53
veronicasilva20 <santinoveronicasilva@gmail.com>
2023-09-04 12:37:38 -0300

    ok:extendi el codigo con propiedades mas

--- script.js
+++ script.js
@@ -2,14 +2,22 @@ const express = require('express');
 const Joi = require('joi'); //used for validation
 const app = express();
 app.use(express.json());
- 
+
 const books = [
-{title: 'Harry Potter', id: 1},
-{title: 'Twilight', id: 2},
-{title: 'Lorien Legacies', id: 3},
-{title: 'libro cuatro nuevo', id:4}
+
+    {title: 'Harry Potter', id: 1, autor: 'J.K. Rowling',ventas:500},
+    {title: 'Twilight', id: 2, autor: 'Stephenie Meyer',ventas:300},
+    {title: 'Lorien Legacies', id: 3, autor: 'Pittacus Lore',ventas:200},
+    {title: 'libro cuatro nuevo', id:4, autor: 'veronica silva',ventas:700},
+    {title: ' Goblet of Fire',id:5, autor:'J.K. Rowling',ventas:270},
+    {tilte: 'Prisoner of Azkaban',id:6,autor:'J.K. Rowling',ventas:510},
+    {title: 'Chamber of Secrets',id:7,autor:'J.K. Rowling',ventas:220},
+    {title: 'Order of the Phoenix',id:8,autor:'J.K. Rowling',ventas:430},
+    {title: 'Half-Blood Prince',id:9, autor:'J.K. Rowling',ventas:600},
+    {title: 'Deathly Hallows',id:10,autor:'J.K. Rowling',ventas:430},
+    {title: 'Philosophers stone',id:11,autor:'J.K. Rowling',ventas:300}
 ]
- 
+
 //READ Request Handlers
 app.get('/', (req, res) => {
 res.send('Welcome to Edurekas REST API with Node.js Tutorial!!');
@@ -25,6 +33,21 @@ const book = books.find(c => c.id === parseInt(req.params.id));
 if (!book) res.status(404).send('<h2 style="font-family: Malgun Gothic; color: darkred;">Ooops... Cant find what you are looking for!</h2>');
 res.send(book);
 });
+
+
+// creo ranking
+
+app.get('/ranking', (req, res) => {
+// Ordenar los libros por ventas de mayor a menor
+    const librosOrdenados = books.sort((a, b) => b.ventas - a.ventas);
+
+// Tomar los primeros 10 libros del ranking
+    const ranking = librosOrdenados.slice(0, 10);
+
+    res.send(ranking);
+});
+    
+
  
 //CREATE Request Handler
 app.post('/api/books', (req, res)=> {
@@ -82,3 +105,7 @@ const schema= Joi.object({ title: Joi.string() .min(6) .required()})
 //PORT ENVIRONMENT VARIABLE
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}..`));
+
+ 
+
+
